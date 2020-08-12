a31df2eae28919141ef8e9730737bb458dcd9fd9
Colabear754 <68015819+Colabear754@users.noreply.github.com>
2020-08-12 17:33:19 +0900

    프리뷰 해상도에 대한 코드 추가

--- app/src/main/java/kr/ac/pknu/perdu/CameraSurfaceView.java
+++ app/src/main/java/kr/ac/pknu/perdu/CameraSurfaceView.java
@@ -15,7 +15,6 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
